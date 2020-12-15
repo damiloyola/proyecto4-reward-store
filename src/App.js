c787b469b0ae7d3c7682b97751b63fcee3401906
damiloyola <damiloyola@gmail.com>
2020-12-15 01:33:34 -0300

    agrego assets y style components para header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import "./App.css";
 import Header from "./components/Header";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    *{
+        box-sizing:border-box;
+        margin:0;
+    }
+    body{
+        
+        font-family: 'Source Sans Pro', sans-serif;
+        
+    }
+`;
+
 function App() {
     return (
         <div className="App">
+            <GlobalStyle />
             <Header />
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,68 @@
 import React from 'react';
+import styled from "styled-components";
+import heroImg from '../assets/header-x1.png'
+import logo from '../assets/aerolab-logo.svg'
+import coin from '../assets/icons/coin.svg'
+const Nav = styled.nav`
+display:flex;
+justify-content:space-between;
+padding:.5%;
+`;
+const Coins= styled.nav`
+cursor:pointer;
+width:40%;
+min-width:75px;
+display:flex;
+justify-content:space-around;
+align-items:center;
+background-color:#EDEDED;
+border-radius:20px;
+padding-left:8px;
 
+img{
+    margin-top:3px;
+
+}
+`;
+const User = styled.div`
+width:20%;
+display:flex;
+justify-content:space-around;
+align-items:center;
+ 
+
+`;
+const Hero = styled.div`
+    width:100%;
+    height:400px;
+    background-image: url(${heroImg});
+    background-size:cover;
+    background-repeat: no-repeat;
+    background-position:center;
+    position:relative;
+`;
+
+const Title= styled.h1`
+    position:absolute;
+    top:60%;
+    left:20%;
+    font-size:64px;
+    color:white;
+`;
 const Header = () =>{
 
-    return <h1></h1>
+    return (<div>
+                <Nav>
+                    <img src={logo} alt="logo aerolab"/>
+                    <User>
+                        <p>Julia Coi</p>
+                        <Coins><span>6000</span> <img src={coin} alt=""/></Coins>
+                    </User>
+                </Nav>
+                <Hero>
+                    <Title>Electronics</Title>
+                </Hero>
+             </div>)
 }
 
 
