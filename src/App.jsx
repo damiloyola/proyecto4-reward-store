bfb5b7240b66d5afe7c2c7a03ffd3f69bf69e3b3
damiloyola <damiloyola@gmail.com>
2021-04-28 14:38:45 -0300

    change to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,23 @@ import Products from "./components/main/Products";
 import Coins from "./components/main/Coins";
 import History from "./components/main/History";
 import Provider from "./components/context/Context";
-import {BrowserRouter as Router,  Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Provider>
-        
-        <Router> 
-          <Header />
-          <main>
-          <Route path= "/" component = {Products} exact/>
-          <Route path= "/coins" component = {Coins} />
-          <Route path = "/history" component = {History}/>
-          </main>
-        </Router>
-        
-      </Provider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Provider>
+                <Router>
+                    <Header />
+                    <main>
+                        <Route path="/" component={Products} exact />
+                        <Route path="/coins" component={Coins} />
+                        <Route path="/history" component={History} />
+                    </main>
+                </Router>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
